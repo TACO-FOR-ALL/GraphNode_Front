e944d3fa251c98bd7874c186a03792a44139cc32
John Han <work.johnhan@gmail.com>
2025-12-12 14:25:33 +0800

    CHORE: 홈화면 동기화 로직 개선 사항 주석 추가

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -5,7 +5,7 @@ import { useEffect } from "react";
 import { pullNotesOnce } from "@/managers/syncNoteWorker";
 
 export default function Home() {
-  // 로그인 후 메인에서 노트 및 채팅 동기화 작업을 진행합니다
+  // 로그인 후 메인에서 노트 및 채팅 동기화 작업을 진행합니다 (로그인 화면에서 진행해야함, 아니면 메인화면 올 때마다 불필요한 요청이 발생)
   useEffect(() => {
     (async () => {
       await pullNotesOnce();
