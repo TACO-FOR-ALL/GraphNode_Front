ee446c606089ae17ec714d9fb95349d325f33965
John Han <work.johnhan@gmail.com>
2026-01-23 14:36:31 +0900

    FEAT: shell을 통해 시스템 기본 브라우저에서 링크 열기 기능 추가

--- electron/main/ipc/system.ts
+++ electron/main/ipc/system.ts
@@ -1,6 +1,9 @@
-import { app, ipcMain } from "electron";
+import { app, ipcMain, shell } from "electron";
 
 // 시스템 언어 가져오기
 export default function systemIPC() {
   ipcMain.handle("system:getLocale", () => app.getLocale());
+  ipcMain.handle("system:openExternal", (_, url: string) =>
+    shell.openExternal(url)
+  );
 }
--- electron/preload/bridges/systemBridge.ts
+++ electron/preload/bridges/systemBridge.ts
@@ -1,9 +1,9 @@
-import { contextBridge } from "electron";
-import { ipcRenderer } from "electron";
+import { contextBridge, ipcRenderer } from "electron";
 
 // 시스템 언어 가져오기
 export default function exposeSystemBridge() {
   contextBridge.exposeInMainWorld("systemAPI", {
     getLocale: () => ipcRenderer.invoke("system:getLocale"),
+    openExternal: (url: string) => ipcRenderer.invoke("system:openExternal", url),
   });
 }
